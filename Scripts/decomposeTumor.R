4b89de40fbb12fd494e02fda37a506a573241047
chris kreitzer <58993468+chris-kreitzer@users.noreply.github.com>
2021-09-10 14:42:33 +0200

    adding lapply

--- Scripts/decomposeTumor.R
+++ Scripts/decomposeTumor.R
@@ -17,9 +17,13 @@ library(tidyverse)
 
 
 #' prepare tumor:
-prepare_tumor_array = function(sample_path, PON_path, threshold = NULL){
+prepare_tumor_array = function(sample_path, PON_path, path_to_save, threshold = NULL){
   message('Please provide a path to a normal (PON) sample [.rds]')
   message('tumor list should contain the column SAMPLE which has the path to sample')
+  
+  if(is.null(threshold)){
+    stop('process is interrupted with no position-threshold', call. = T)
+  }
   
   #' marker positions which are used for the creation of the PON
   PON_used = readRDS(PON_path)
@@ -66,11 +70,11 @@ prepare_tumor_array = function(sample_path, PON_path, threshold = NULL){
   NA_tumors = c()
   for(i in unique(data_merged$sample)){
     tumor_sample_out = data_merged[which(data_merged$sample == i & data_merged$duplication %in% Markers_used$merged_position), ]
-    if(dim(tumor_sample_out)[1] / dim(Markers_used)[1] <= 0.95){
+    if(dim(tumor_sample_out)[1] / dim(Markers_used)[1] <= threshold){
       print(paste0('Sample ', i, 'cannot be used for estimation, because too little bins'))
       NA_tumors = c(NA_tumors, i)
       next
-    } else if (dim(tumor_sample_out)[1] / dim(Markers_used)[1] > 0.95){
+    } else if (dim(tumor_sample_out)[1] / dim(Markers_used)[1] > threshold){
       missing = setdiff(Markers_used$merged_position, data_merged$duplication[which(data_merged$sample == i)])
       if(length(missing) == 0){
         extracted_tumors = rbind(extracted_tumors, tumor_sample_out)
@@ -89,7 +93,52 @@ prepare_tumor_array = function(sample_path, PON_path, threshold = NULL){
   }
   
   #' make the mean normalization, create GRobject and save the output for analysis
-  for(i in unique())
+  output_tumors = function(data, path_to_save){
+    data$normalized.depth = data$depth / mean(data$depth)
+    samplename = unique(data$sample)
+    print(samplename)
+    data = data[, c('Chromosome', 'Position', 'normalized.depth', 'sample')]
+    Tumor_GR = data.frame(seqnames = data$Chromosome,
+                          start = data$Position,
+                          end = data$Position + 1,
+                          reads.corrected = data$normalized.depth)
+    print(head(Tumor_GR))
+    Tumor_GRobject = makeGRangesFromDataFrame(df = Tumor_GR, keep.extra.columns = T)
+    saveRDS(Tumor_GRobject, file = paste0(path_to_save, samplename, '.rds'))
+    
+  }
+  
+lapply(unique(extracted_tumors$sample), function(x) output_tumors(data = extracted_tumors, path_to_save = '~/Desktop/'))
+  
+  path_to_save = '~/Desktop/'
+
+  #' append one nucleotide, ot have a proper range object
+    GR_sample = resize(GR_sample, width(GR_sample) + 1, fix = 'start')
+    saveRDS(GR_sample, file = paste0(path_to_save, 'sample', i, '.rds'))
+    
+    #' prepare data table for subsequent follow-up
+    paths = data.table::data.table(sample = paste0('sample', i),
+                                   normal_cov = paste0('~/Documents/MSKCC/07_FacetsReview/PON_BRCA/sample', i, '.rds'))
+    PON_path = rbind(PON_path, paths)
+  }
+  saveRDS(PON_path, file = 'PON_BRCA/normal_table.rds')
+}
+
+modify_PON(data = PON, path_to_save = 'PON_BRCA/')
+
+  
+  
+  summarize_data <- function(a_csv, the_dir) {
+    # open the data, fix the date and add a new column
+    the_data <- read.csv(a_csv, header = TRUE, na.strings = 999.99) %>%
+      mutate(DATE = as.POSIXct(DATE, tz = "America/Denver", format = "%Y-%m-%d %H:%M:%S"),
+             # add a column with precip in mm - you did this using a function previously
+             precip_mm = (HPCP * 25.4))
+    
+    # write the csv to a new file
+    write.csv(the_data, file = paste0(the_dir, "/", basename(a_csv)),
+              na = "999.99")
+  }
   
   
   #' prepare the final output
