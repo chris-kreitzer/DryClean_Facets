050d17353ab339fc13f96b0e97b367edd2b6309f
chris kreitzer <58993468+chris-kreitzer@users.noreply.github.com>
2021-09-12 11:13:52 +0200

    transformation

--- Scripts/DryClean_IMPACT.R
+++ Scripts/DryClean_IMPACT.R
@@ -2,12 +2,32 @@
 ## 
 
 
+library(GenomicRanges)
+#' read the rds object - than convert to GRanges object - then run it
+a = GRanges(as.data.frame(GRangesList(cov)))
 
 
 
+cov = readRDS('~/Desktop/countsMerged____P-0002273-T01-IM3_P-0002273-N01-IM3.dat.gz.rds')
+
+
+
+
+cov_out = dryclean::start_wash_cycle(cov = a,
+                                     detergent.pon.path = '~/Documents/MSKCC/07_FacetsReview/PON_BRCA/detergent.rds',
+                                     whole_genome = F,
+                                     chr = NA,
+                                     germline.filter = FALSE, mc.cores = 4)
+
+
+cov
 cov_out = start_wash_cycle(cov = coverage_file, 
                            detergent.pon.path = "~/git/dryclean/inst/extdata", 
                            whole_genome = TRUE, 
                            chr = NA, 
                            germline.filter = TRUE, 
                            germline.file = "~/git/dryclean/inst/extdata/germline.markers.rds")
+typeof(cov)
+
+a
+
