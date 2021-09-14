36fa04272b088581ffd3c4bde8a8b2f1432c9f4d
chris kreitzer <58993468+chris-kreitzer@users.noreply.github.com>
2021-09-14 23:48:36 +0200

    new

--- Scripts/DryClean_IMPACT.R
+++ Scripts/DryClean_IMPACT.R
@@ -4,22 +4,19 @@
 
 library(GenomicRanges)
 #' read the rds object - than convert to GRanges object - then run it
+#' fetch one converted sample from juno script;
+
 cov = readRDS('~/Desktop/countsMerged____P-0002273-T01-IM3_P-0002273-N01-IM3.dat.gz.rds')
 
+#' convert object
 a = GRanges(as.data.frame(GRangesList(cov)))
-
-
-
-
-
-
-
 
 cov_out = dryclean::start_wash_cycle(cov = a,
                                      detergent.pon.path = '~/Documents/MSKCC/07_FacetsReview/PON_BRCA/detergent.rds',
                                      whole_genome = F,
                                      chr = NA,
                                      germline.filter = FALSE, mc.cores = 4)
+
 
 
 # Loading PON a.k.a detergent from path provided
