f9158cb494b669bf603a3909087ae5ae8abb6bf8
chris kreitzer <58993468+chris-kreitzer@users.noreply.github.com>
2021-09-15 18:52:54 +0200

    DryClean processing

--- Scripts/DryClean_IMPACT.R
+++ Scripts/DryClean_IMPACT.R
@@ -21,18 +21,6 @@ for(i in test){
 
 
 
-cov = readRDS('~/Desktop/countsMerged____P-0002273-T01-IM3_P-0002273-N01-IM3.dat.gz.rds')
-
-#' convert object
-a = GRanges(as.data.frame(GRangesList(cov)))
-
-cov_out = dryclean::start_wash_cycle(cov = a,
-                                     detergent.pon.path = '~/Documents/MSKCC/07_FacetsReview/PON_BRCA/detergent.rds',
-                                     whole_genome = F,
-                                     chr = NA,
-                                     germline.filter = FALSE, mc.cores = 4)
-
-
 
 # Loading PON a.k.a detergent from path provided
 # Let's begin, this is whole exome/genome
@@ -44,13 +32,5 @@ cov_out = dryclean::start_wash_cycle(cov = a,
 # Error in m.vec - s : non-conformable arrays
 
 
-
-cov
-cov_out = start_wash_cycle(cov = coverage_file, 
-                           detergent.pon.path = "~/git/dryclean/inst/extdata", 
-                           whole_genome = TRUE, 
-                           chr = NA, 
-                           germline.filter = TRUE, 
-                           germline.file = "~/git/dryclean/inst/extdata/germline.markers.rds")
 
 
