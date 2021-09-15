5268675a4f8c780b587ff66d3d36ceec9745ccaf
chris kreitzer <58993468+chris-kreitzer@users.noreply.github.com>
2021-09-15 18:25:59 +0200

    dryclean automatisation

--- Scripts/DryClean_IMPACT.R
+++ Scripts/DryClean_IMPACT.R
@@ -3,8 +3,23 @@
 
 
 library(GenomicRanges)
-#' read the rds object - than convert to GRanges object - then run it
-#' fetch one converted sample from juno script;
+setwd('~/Documents/GitHub/DryClean_Facets/Tumor_samples/')
+Tumor_samples = list.files(getwd(), full.names = T)
+
+test = Tumor_samples[1:2]
+
+for(i in test){
+  cov = readRDS(i)
+  cov_converted = GRanges(as.data.frame(GRangesList(cov)))
+  cov_out = dryclean::start_wash_cycle(cov = cov_converted,
+                                       detergent.pon.path = '~/Documents/GitHub/DryClean_Facets/detergent.rds',
+                                       whole_genome = F,
+                                       chr = NA,
+                                       germline.filter = FALSE,
+                                       mc.cores = 4)
+}
+
+
 
 cov = readRDS('~/Desktop/countsMerged____P-0002273-T01-IM3_P-0002273-N01-IM3.dat.gz.rds')
 
