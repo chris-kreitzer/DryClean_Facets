b0623e06ac3cb2d6f0e9f351d04a3acf28d1c71a
chris kreitzer <58993468+chris-kreitzer@users.noreply.github.com>
2021-09-17 08:59:05 +0200

    new

--- Scripts/identifyGermline.R
+++ Scripts/identifyGermline.R
@@ -1,16 +1,26 @@
 ## Identify germline alteration from the normals;
 ## 
-## 
+## working with default parameters
+## input is a table which contains the normals as well as the 
+## decomposed normals from start_wash_cycle()
+## importantly - normals are treated like tumors
+
+## 09/17/2021
+## chris-kreitzer
 
 
-#' normal.table.path must contain 
-#' sample	
-#' normal_cov	
-#' decomposed_cov (from identify germline)
-#' start_wash_cycle
+library(parallel)
+library(dryclean)
 
 
+## the input data table must be in .rds format
+## moreover it must be a data.table::data.table() object
+## columns that are required
+#' sample
+#' normal_cov
+#' decomposed_cov
 
+working_path = ' '
 
 Germline = identify_germline(normal.table.path = paste0(working_path, 'decomposed_samples/normal_table.rds'), 
                              path.to.save = paste0(working_path, 'decomposed_samples'), 
