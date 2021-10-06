c7436b3fbd2c6a553a6ec58f202d15dd106b1c52
chris kreitzer <58993468+chris-kreitzer@users.noreply.github.com>
2021-10-06 18:57:33 +0200

    added script

---
+++ Scripts/NoiseReduction.R
@@ -0,0 +1,12 @@
+## Extract pivotal information from Facets calls; and compare 
+## pre- post DryClean outputs; find marker postions where DryClean makes an impact
+## 
+## 10/06/2021: original script
+## chris-kreitzer
+## 
+
+
+
+## Take the T/N ratio of the same positions as used for DryClean and look into
+## the distribution (deviation; noise reduction)
+
