2bfdc377c4240d810fc376dda635225536ccc5f4
chris kreitzer <58993468+chris-kreitzer@users.noreply.github.com>
2021-10-12 12:33:23 +0200

    update

--- Scripts/NoiseQuantification.R
+++ Scripts/NoiseQuantification.R
@@ -23,13 +23,44 @@ colnames(RecognizedProbes)[2] = 'start'
 RecognizedProbes$probes = paste(RecognizedProbes$chromosome, RecognizedProbes$start, sep = ';')
 
 #' ROI
-ROI = data.frame(chromosome = c(17, ))
+ROI = data.frame(chromosome = c(17, 8, 1),
+                 start = c(27229517, 1300569, 131354907),
+                 end = c(48441764, 44119298, 239198818),
+                 gene = c('ERBB2', '8p_arm', '1q_arm'))
+
+
 #' TN metrics;
 TN_metrics = function(data){
+  file_name = basename(data)
   countdata = facets::readSnpMatrix(data, err.thresh = 10, del.thresh = 10)
-  countdata$probes = paste(countdata$Chromosome, countdata$Position)
+  countdata$probes = paste(countdata$Chromosome, countdata$Position, sep = ';')
+  countdata = countdata[which(countdata$Chromosome == ROI[1, 'chromosome']), ]
+  countdata = countdata[which(countdata$probes %in% RecognizedProbes$probes[which(RecognizedProbes$chromosome == ROI[1, 'chromosome'])]), ]
+  countdata = countdata[which(countdata$Position > ROI[1, 'start'] &
+                                countdata$Position < ROI[1, 'end']), ]
+  countdata$TN_ratio = log(countdata$TUM.DP / countdata$NOR.DP)
+  countdata$indx = seq(from = 1, to = nrow(countdata), by = 1)
+  countdata$sample = file_name
+  countdata$dispersion = dlrs(x = countdata$TN_ratio)
+  countdata
 }
 
-sample1 = facets::readSnpMatrix('Tumor_countsFile/countsMerged____P-0000584-T03-IM6_P-0000584-N01-IM6.dat.gz')
-head(sample1)
-head(RecognizedProbes)
+
+files = list.files('Tumor_countsFile/', full.names = T, all.files = F, include.dirs = T)
+files = paste0('/Users/chriskreitzer/Documents/GitHub/DryClean_Facets/', files)
+
+
+x = lapply(files, function(x) TN_metrics(x))
+y = data.table::rbindlist(x)
+
+#' 
+
+
+
+
+
+
+
+
+
+
