5013d0c02080442e644daae0d49bf4a2660338a5
chris kreitzer <58993468+chris-kreitzer@users.noreply.github.com>
2021-10-23 23:03:48 +0200

    closed script

--- Scripts/MODE1_comparision.R
+++ Scripts/MODE1_comparision.R
@@ -6,17 +6,6 @@
 ## chris-kreitzer
 ## 
 
-rm(list = ls())
-.rs.restartR()
-set.seed(100)
-setwd('~/Documents/GitHub/DryClean_Facets/Scripts/DryClean_Segmentation/facets-0.6.1/')
-
-
-#' testing the updated package
-install.packages('~/Documents/GitHub/DryClean_Facets/Scripts/DryClean_Segmentation/facets-0.6.1/', 
-                 type = "source", repos = NULL)
-
-library(FacetsDC)
 
 #' downstream
 setwd('~/Documents/GitHub/DryClean_Facets/')
