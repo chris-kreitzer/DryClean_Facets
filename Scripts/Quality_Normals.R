cf68473ba000f6a661b86d01414ac73749d7661a
chris kreitzer <58993468+chris-kreitzer@users.noreply.github.com>
2021-10-29 10:14:32 +0200

    normal quality

--- Scripts/Quality_Normals.R
+++ Scripts/Quality_Normals.R
@@ -1,6 +1,10 @@
 ## Normal quality: Assessing the suitability of Normals
 ## 
 
+
+#' Data which is required:
+#' Facets_original_snps - 
+
 plot_list = list()
 for(i in unique(Facets_original_snps$name)){
   dat = Facets_original_snps[which(Facets_original_snps$name == i), ]
@@ -16,4 +20,10 @@ for(i in unique(Facets_original_snps$name)){
     theme_base(base_size = 14)
   plot_list[[i]] = plot
 }
+
+
+
+a = Facets_original_snps[which(Facets_original_snps$name == 'P-0000584-T03-IM6_P-0000584-N01'), ]
+b = DryClean_snps[which(DryClean_snps$name == 'P-0000584-T03-IM6_P-0000584-N01'), ]
+
 
