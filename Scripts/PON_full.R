f5322cea6debe03a46d39c7a168d3590183c9ca6
chris kreitzer <58993468+chris-kreitzer@users.noreply.github.com>
2022-02-01 20:33:03 +0100

    update

--- Scripts/PON_full.R
+++ Scripts/PON_full.R
@@ -1,2 +1,27 @@
+## Create a Panel of Normals (PON)
+##
+## Learn, whether different tools provide us with different count matrices
+## Learn, about the noise in the input data
+## Learn, whether a reduced AND/OR binned PON make a difference
+##
+## 02/01/2022
+## chris-kreitzer
 
-setwd('~/Documents/GitHub/DryClean_Facets/')
+
+clean()
+setup(working.path = '~/Documents/GitHub/DryClean_Facets/')
+
+
+## Libraries and Dependencies:
+devtools::install_github('mskilab/dryclean')
+# devtools::install_github('mskilab/gUtils')
+devtools::install_github("mskilab/bamUtils")
+BiocManager::install('S4Vectors')
+BiocManager::install('GenomicAlignments')
+library(S4Vectors)
+library(gUtils)
+library(dryclean)
+library(tidyverse)
+library(pbmcapply)
+library(data.table)
+
