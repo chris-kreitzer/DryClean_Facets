d20e6c0ca4630056979f82d0940b838d89eb467b
chris kreitzer <58993468+chris-kreitzer@users.noreply.github.com>
2022-02-04 17:28:44 +0100

    CI

--- Scripts/UtilityFunctions.R
+++ Scripts/UtilityFunctions.R
@@ -19,3 +19,19 @@ dlrs = function(x) {
 ggsave_golden = function(filename, plot, width, ...){
   ggsave(filename = filename, plot = plot, device = cairo_pdf, width = width, height = width / 1.61803398875)
 }
+
+CI_z <- function (x, ci = 0.95){
+  `%>%` <- magrittr::`%>%`
+  standard_deviation = sd(x)
+  sample_size = length(x)
+  Margin_Error = abs(qnorm((1-ci)/2))* standard_deviation/sqrt(sample_size)
+  df_out = data.frame(sample_size = length(x), 
+                      Mean = mean(x), 
+                      sd=sd(x),
+                      Margin_Error = Margin_Error,
+                      'CI lower limit'= (mean(x) - Margin_Error),
+                      'CI Upper limit'= (mean(x) + Margin_Error)) %>%
+    tidyr::pivot_longer(names_to = "Measurements", 
+                        values_to = "values", 1:6)
+  return(df_out)
+}
