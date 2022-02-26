e9537a5c0d64458bf52985684aac408968225ded
chris kreitzer <58993468+chris-kreitzer@users.noreply.github.com>
2022-02-26 17:00:27 +0100

    DryClean()

--- Scripts/DryClean_Tumor_Cluster.R
+++ Scripts/DryClean_Tumor_Cluster.R
@@ -1,34 +1,3 @@
-## Running decompostion (DryClean) on tumor samples
-## start_wash_cycle()
-## 
-## Start: 09/18/2021
-## Revision: 02/26/2022
-## chris-kreitzer
-
-
-clean()
-library(GenomicRanges)
-setwd('~/Documents/GitHub/DryClean_Facets/')
-
-
-Tumor_samples = list.files('~/Documents/MSKCC/07_FacetsReview/DryClean/TUMOR_BRCA/', full.names = T)
-
-test = Tumor_samples[1:2]
-
-for(i in test){
-  cov = readRDS(i)
-  cov_converted = GRanges(as.data.frame(GRangesList(cov)))
-  cov_out = dryclean::start_wash_cycle(cov = cov_converted,
-                                       detergent.pon.path = '~/Documents/GitHub/DryClean_Facets/detergent.rds',
-                                       whole_genome = F,
-                                       chr = NA,
-                                       germline.filter = FALSE,
-                                       mc.cores = 4)
-  saveRDS(cov_out, file = paste0('~/Desktop/', basename(i), '.rds'))
-}
-
-
-##-----------------------------------------------------------------------------
 ## Cluster Version
 
 require('dryclean', '/juno/home/kreitzec/R/x86_64-pc-linux-gnu-library/4.0/')
