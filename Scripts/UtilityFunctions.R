14321b67c67f978ee2eb17a41485c488a63c5cda
chris kreitzer <58993468+chris-kreitzer@users.noreply.github.com>
2022-03-02 17:00:44 +0100

    updated utility funciton

--- Scripts/UtilityFunctions.R
+++ Scripts/UtilityFunctions.R
@@ -35,3 +35,44 @@ CI_z <- function (x, ci = 0.95){
                         values_to = "values", 1:6)
   return(df_out)
 }
+
+
+## retrieve exonic information of specific genes
+exonic_structure = function(gene, type){
+  library(bamUtils)
+  library(rtracklayer)
+  library(GenomicRanges)
+  
+  #' check if genecode file is already loaded in environment
+  if('genecode' %in% ls(environment())){
+    genecode = genecode
+    message('Gencode annotation already loaded')
+  } else {
+    if(file.exists('~/Documents/GitHub/DryClean_Facets/gencode.v19.chr_patch_hapl_scaff.annotation.gtf.gz')){
+      genecode = rtracklayer::import('~/Documents/GitHub/DryClean_Facets/gencode.v19.chr_patch_hapl_scaff.annotation.gtf.gz')
+      message('Gencode annotation needs to be loaded')
+    } else {
+      stop('Gencode-file does not exist anymore in destination: ~/Documents/GitHub/DryClean/DryClean_Facets/gencod.v19...', call. = T)
+    }
+  }
+  
+  #' retrieve Gene and Type
+  selected_gene = genecode %Q% (gene_name == gene)
+  feature_out = as.data.frame(selected_feature)
+  feature_out = feature_out[which(feature_out$type == type), ]
+  
+  return(feature_out)
+  
+}
+
+# file.edit(file.path("~", ".Rprofile"))
+
+
+
+
+
+
+
+
+
+
