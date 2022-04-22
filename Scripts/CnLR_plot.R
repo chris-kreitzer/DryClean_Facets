03c848f7f2762a15c304bc3cb244924c37f43867
chris kreitzer <christoph.kreitzer93@gmx.at>
2022-04-22 19:37:12 +0200

    new scripts added

---
+++ Scripts/CnLR_plot.R
@@ -0,0 +1,81 @@
+##-----------------------------------------------------------------------------
+## Customized CnLR plot: based on FacetsSuite
+##
+## start: 04/22/2022
+## 
+## chris-kreitzer
+
+
+source('~/Documents/GitHub/MSKCC/Scripts/hg19.R')
+
+
+cnlr_plot(facets_data = facets_data, genome = hg19)
+
+#' function
+cnlr_plot = function(facets_data,
+                     colors = c('#5c9e75', '#272425'),
+                     genome = c('hg19', 'hg18', 'hg38')) {
+  
+  genome = genome
+  
+  snps = facets_data$snps
+  segs = facets_data$segs
+  dipLogR = facets_data$dipLogR
+  
+  snps = get_cum_chr_maploc(snps, genome)
+  mid = snps$mid[names(snps$mid) %in% snps$snps$chrom]
+  centromeres = snps$centromeres
+  snps = snps$snps
+  
+  snps$cnlr_median = rep(segs$cnlr.median, segs$num.mark)
+  
+  starts = cumsum(c(1, segs$num.mark))[seq_along(segs$num.mark)]
+  ends = cumsum(c(segs$num.mark))
+  my_starts = snps[starts, c('chr_maploc', 'cnlr_median')]
+  my_ends = snps[ends, c('chr_maploc', 'cnlr_median')]
+  
+  ymin = floor(min(segs$cnlr.median, na.rm = T))
+  ymax = ceiling(max(segs$cnlr.median, na.rm = T))
+  if (ymin > -5) ymin = -5
+  if (ymax < 5) ymax = 5
+  
+  
+  pt_cols = colors[c(snps$chrom %% 2) + 1]
+  
+  # plot
+  cnlr = ggplot(snps) +
+    geom_point(aes(y = cnlr, x = chr_maploc), pch = 19, col = pt_cols, size = .4) +
+    scale_x_continuous(breaks = mid, labels = names(mid), expand = c(0, 0)) +
+    scale_y_continuous(breaks = scales::pretty_breaks(), limits = c(ymin, ymax), expand = c(0,0)) +
+    geom_hline(yintercept = dipLogR, color = 'sandybrown', size = .8) +
+    geom_segment(data = segs, aes(x = my_starts$chr_maploc, xend = my_ends$chr_maploc,
+                                  y = my_starts$cnlr_median, yend = my_ends$cnlr_median),
+                 col = 'red3', size = 1, lineend = 'butt') +
+    labs(x = NULL, y = 'Copy number log ratio') +
+    theme_bw() +
+    theme(axis.text.x = element_text(angle = 0, size = 10, color = 'black'),
+          axis.text.y = element_blank(),
+          text = element_text(size = 12),
+          axis.ticks.y = element_blank(),
+          panel.grid.minor = element_line(colour = 'grey', size = 0.2),
+          panel.grid.major = element_blank(),
+          panel.border = element_rect(fill = NA, size = 1.5))
+  return(cnlr)
+  
+}
+
+
+
+get_cum_chr_maploc = function(snps, genome = hg19) {
+  
+  genome = genome
+  
+  cum_chrom_lengths = cumsum(as.numeric(genome$size))
+  mid = cum_chrom_lengths - (genome$size / 2)
+  names(mid) = seq_len(nrow(genome))
+  centromeres = genome$centromere + c(0, cum_chrom_lengths[-length(cum_chrom_lengths)])
+  
+  snps$chr_maploc = snps$maploc + c(0, cum_chrom_lengths)[snps$chrom]
+  
+  list(snps = snps, mid = mid, centromeres = centromeres)
+}
---
+++ Scripts/waterfallPattern.R
@@ -0,0 +1,104 @@
+##-----------------------------------------------------------------------------
+## Check WATERFALL pattern:
+##-----------------------------------------------------------------------------
+##
+## start: 04/22/2022
+## chris-kreitzer
+
+
+clean()
+gc()
+source('~/Documents/GitHub/MSKCC/Scripts/hg19.R')
+library(facets)
+library(pctGCdata)
+library(facetsSuite)
+library(patchwork)
+
+## P-0018672-T01-IM6
+#' original
+countmatrix = facets::readSnpMatrix('~/Desktop/tmp/countsMerged____P-0018672-T01-IM6_P-0018672-N01-IM6.dat.gz')
+facets_original = facetsSuite::run_facets(read_counts = countmatrix,
+                                          cval = 100,
+                                          snp_nbhd = 250,
+                                          genome = 'hg19', seed = 100)
+
+original = cnlr_plot(facets_data = facets_original, genome = hg19)
+
+
+#' Cleaned
+cleaned = FacetsDC::run_facets_cleaned(read_counts = '~/Desktop/tmp/countsMerged____P-0018672-T01-IM6_P-0018672-N01-IM6.dat.gz', 
+                                       read_cleaned = '~/Desktop/tmp/sample249.rds.rds', 
+                                       MODE = 'union', 
+                                       cval = 100, 
+                                       seed = 100, snp_nbhd = 250)
+
+cleaned_plot = cnlr_plot(facets_data = cleaned, genome = hg19)
+
+original / cleaned_plot
+
+## other way for visualization: 
+plot(density(facets_original$snps$cnlr),
+     xaxt = 'n', las = 2, 
+     xlab = '',
+     main = '',
+     ylim = c(0, 0.8),
+     lwd = 2,
+     col = 'black')
+axis(1, at = c(-4, -2, 0, 2, 4))
+lines(density(cleaned$snps$cnlr),
+      lwd = 2, col = 'red')
+box(lwd = 2)
+mtext(side = 1, text = 'CnLR', line = 1.8, cex = 1.3)
+text(x = 1.9, y = 0.4, label = '(drycleaned-) CnLR\ndlrs=0.75', col = 'red', font = 2)
+text(x = -1.8, y = 0.3, label = '(raw) CnLR\ndlrs=1.44', col = 'black', font = 2)
+
+dlrs(facets_original$snps$cnlr)
+dlrs(cleaned$snps$cnlr)
+
+
+##-----------------------------------------------------------------------------
+## Systematic evaluation of the waterfall pattern: 
+## ----------------------------------------------------------------------------
+MasterFile = readRDS('~/Documents/MSKCC/07_FacetsReview/DryClean/TUMOR_BRCA/tumor_table.rds')
+
+
+countfiles = list.files('~/Documents/MSKCC/07_FacetsReview/DryClean/Tumor_Countfiles/', full.names = T)
+cleanedfiles = list.files('~/Documents/MSKCC/07_FacetsReview/DryClean/TUMOR_DECOMPOSED/', full.names = T)
+
+waterfall_out = data.frame()
+for(i in 1:nrow(MasterFile)){
+  try({
+    print(MasterFile$original[i])
+    count = facetsSuite::read_snp_matrix(input_file = grep(pattern = MasterFile$original[i], x = countfiles, value = T))
+    facets_out = facetsSuite::run_facets(read_counts = count, cval = 100, genome = 'hg19', seed = 100)
+    facets_dlrs = dlrs(x = facets_out$snps$cnlr)
+    
+    #' cleaned
+    cleaned = FacetsDC::run_facets_cleaned(read_counts = grep(pattern = MasterFile$original[i], x = countfiles, value = T),
+                                           read_cleaned = grep(pattern = paste0(MasterFile$sample[i], '.rds'), x = cleanedfiles, value = T),
+                                           MODE = 'union', cval = 100, seed = 100)
+    cleaned_dlrs = dlrs(x = cleaned$snps$cnlr)
+    
+    waterfall = data.frame(sample = MasterFile$original[i],
+                          facets_dlrs = facets_dlrs,
+                          dryclean_dlrs = cleaned_dlrs)
+    
+    waterfall_out = rbind(waterfall_out, waterfall)
+  })
+  
+}
+
+ggplot(waterfall_out) +
+  geom_jitter(aes(y = facets_dlrs, x = 1), size = 0.65, width = 0.25) +
+  geom_jitter(aes(y = dryclean_dlrs, x = 2), width = 0.25, size = 0.65) +
+  scale_y_continuous(expand = c(0.05, 0.01), limits = c(0, 5.5)) +
+  scale_x_continuous(expand = c(0.05, 0.01), labels = c('Facets\n(raw)', 'Facets\n(cleaned)'), breaks = c(1,2)) +
+  theme_bw() +
+  theme(aspect.ratio = 1.3,
+        axis.text = element_text(size = 12, colour = 'black'),
+        axis.title = element_text(size = 12, colour = 'black'),
+        panel.border = element_rect(fill = NA, size = 1.3, color = 'black'),) +
+  annotate('text', x = 1.5, y = 5.5, label = '(paired t-test) p<2.2e-16') +
+  labs(x = '', y = 'dlrs [CnLR]')
+
+t.test(waterfall_out$facets_dlrs, waterfall_out$dryclean_dlrs, paired = T)
