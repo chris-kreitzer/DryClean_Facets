3f838ad5b8adef58e3f2119e34173e532cdaee9c
chris kreitzer <christoph.kreitzer93@gmx.at>
2022-06-06 13:09:30 +0200

    updated script

--- Scripts/CnLR_plot.R
+++ Scripts/CnLR_plot.R
@@ -14,9 +14,15 @@ source('~/Documents/GitHub/MSKCC/Scripts/hg19.R')
 #' function
 cnlr_plot = function(facets_data,
                      colors = c('#5c9e75', '#272425'),
-                     genome = c('hg19', 'hg18', 'hg38')) {
+                     highlight_gene = NULL) {
   
-  genome = genome
+  if('hg19' %in% ls(name = globalenv())){
+    print('hg19 gene model is used')
+  } else {
+      stop('hg19 not loaded in current environment')
+  }
+  
+  genome = hg19
   
   snps = facets_data$snps
   segs = facets_data$segs
@@ -60,6 +66,21 @@ cnlr_plot = function(facets_data,
           panel.grid.minor = element_line(colour = 'grey', size = 0.2),
           panel.grid.major = element_blank(),
           panel.border = element_rect(fill = NA, size = 1.5))
+  
+  
+  # if highligthing gene
+  if (!is.null(highlight_gene)) {
+    if (is.character(highlight_gene)) {
+      highlight_gene = get_gene_position(highlight_gene)
+    }
+    snps$gene = FALSE
+    snps$gene[which(snps$chrom %in% highlight_gene$chrom &
+                      snps$chr_maploc >= highlight_gene$start &
+                      snps$chr_maploc <= highlight_gene$end)] = TRUE
+    cnlr = cnlr +
+      geom_vline(xintercept = highlight_gene$mid, color = 'palevioletred1') +
+      geom_point(data = filter(snps, gene == TRUE), aes(y = cnlr, x = chr_maploc), color = '#525252', size = .4) 
+  }
   return(cnlr)
   
 }
@@ -78,4 +99,31 @@ get_cum_chr_maploc = function(snps, genome = hg19) {
   snps$chr_maploc = snps$maploc + c(0, cum_chrom_lengths)[snps$chrom]
   
   list(snps = snps, mid = mid, centromeres = centromeres)
+}
+
+
+#' extract gene position
+get_gene_position = function(genes) {
+  
+  if('genes_hg19' %in% ls(name = globalenv())){
+    message('genes_hg19 are used')
+  } else {
+    stop('No gene_models are loaded in global environment (eg. genes_hg19')
+  }
+  
+  gene_loci = genes_hg19 
+  genome = hg19
+  cum_chrom_lengths = cumsum(as.numeric(genome$size))
+  
+  if(!all(genes %in% gene_loci$gene)) {
+    missing_genes = setdiff(genes, gene_loci$gene)
+    stop(paste('Gene(s)', paste(missing_genes, collapse = ', '), 'are not in gene annotation.'), call. = FALSE)
+  } else {
+    filter(gene_loci, gene %in% genes) %>%
+      mutate(chrom = as.numeric(chrom),
+             mid = start + (end-start) / 2,
+             start = start + cum_chrom_lengths[chrom-1],
+             end = end + cum_chrom_lengths[chrom-1],
+             mid = mid + cum_chrom_lengths[chrom-1]) 
+  }
 }
